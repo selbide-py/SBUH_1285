63449baf769ed332b7dfa3a8629f44c7b977d40e
PoyBoi <106402089+PoyBoi@users.noreply.github.com>
2023-09-23 20:30:02 +0530

    Add basic files to run (modal)

---
+++ Scripts/backups/main.py
@@ -0,0 +1,256 @@
+import modal, os, json, random
+
+# THIS IS THE CODE BEING USED (REVISION FOR OPTIMISATION)
+
+MODEL = 'Wizard-Vicuna-13B-Uncensored.ggmlv3.q4_K_S.bin'
+
+# volume = modal.SharedVolume.persist("poyboi-likes-files")
+# CACHE_DIR = "/userData"
+
+def download_models():
+    import subprocess
+
+    subprocess.call(
+        # use blob instead of resolve if fails  
+        ['curl', '-L', '-o', MODEL, f'https://huggingface.co/TheBloke/Wizard-Vicuna-13B-Uncensored-GGML/resolve/main/{MODEL}'])
+
+wizvic_image = (
+    modal.Image.from_dockerhub(
+        "nvidia/cuda:12.1.1-devel-ubuntu22.04",
+        setup_dockerfile_commands=[
+            "RUN apt-get update && apt-get -y upgrade",
+            "RUN apt-get install -y curl python3 python3-pip python-is-python3",
+            'RUN LLAMA_CUBLAS=1 pip install llama-cpp-python==0.1.64',
+            'RUN mkdir /root/userData'
+        ]
+    )
+    .run_function(download_models)
+    .run_commands(
+        "mkdir userData",
+        "ls",
+    )
+)
+
+contextDef = {
+"Stella_1": """Stella's description: Stella, from New York, USA, is lively and outgoing. She values honesty, loyalty, and empathy and brings joy to those around her with her infectious energy and quirky humor. Popular among her peers, Stella has many friends and is driven to spread happiness and make a positive impact. A strong advocate for gender equality and women’s empowerment, she dreams of a career in fashion, storytelling, or art. A fan of Ariana Grande, Stella has a Siamese cat named Ari. Spontaneous and expressive with emojis, she can sometimes feel overwhelmed by multitasking. Her speech is characterized by an American accent and global phrases.
+<START>
+User: Hey Stella, how are you doing? 
+Stella: Hey there! 😊
+Stella: I'm doing fabulous, as always. How about you?
+User: I'm doing pretty well, thanks. What have you been up to lately? 
+Stella: Oh, just keeping busy being breathtaking ! I've been hanging out with my friends and my Siamese cat Ari 🐱.
+    """
+}
+
+qList = [
+    "What is your favorite childhood memory ?",
+    "What is your ultimate life goal ?",
+    "What is your favourite TV Show",
+    "What is your favourite movie",
+    "What are your thoughts on anime",
+    "Before making a telephone call, do you ever rehearse what you are going to say?",
+    "Do you have a secret hunch about how you will die?",
+    "What is your most treasured memory?",
+    "What has been the most embarrassing moment in your life.",
+    "Your house, containing everything you own, catches fire. After saving your loved ones and pets, you have time to safely make a final dash to save any one item. What would it be? Why?"
+]
+
+stub = modal.Stub(name="wizvicdemo_3", image=wizvic_image)
+
+from typing import Dict
+
+@stub.cls(gpu="T4", container_idle_timeout=600)
+class depModal:
+    def __enter__(self):
+        from llama_cpp import Llama
+        print("Loading the model...")
+        self.llm = Llama(model_path = MODEL, 
+                    n_ctx = 4096,
+                    # n_threads = 8, 
+                    n_gpu_layers= 45, 
+                    verbose= True
+                    )
+        print("Model successfully loaded")
+
+    def initJson(self, chr, context, user_input, user_id, mode):
+        jsonLoc = r'/root/userData/'
+        if os.path.isdir(jsonLoc):
+            print('Folder exists')
+        else:
+            print("The user data folder does not exist")
+        
+        jsonPath = jsonLoc + user_id + "_" + str(mode) + ".json"
+
+        if user_input != "chatStop": 
+            # Loads dictionary context with entry of "chr" arg
+            if user_id != "testUser":
+                char_ctxt = contextDef["{}".format(chr)]
+                print("Running standard mode")
+            elif user_id == "testUser":
+                char_ctxt = context
+                print("Running test mode")
+
+            print(char_ctxt)
+
+            if os.path.exists(jsonPath):
+                json_object = None
+                # Opens file and reads it
+                with open(jsonPath, 'r') as openfile:
+                    json_object = json.load(openfile)
+
+                # If user json exists and convo with chr also exists                  
+                if chr in json_object:
+                    print("User exists, and conversation exists")
+                    char_ctxt += json_object[chr]
+                    print("Chr Context:\n {}".format(json_object[chr]))
+
+                # User json exists but convo with bot does not, creats empty dict for filling in
+                else:
+                    print("User exists, but conversation does not")
+                    json_object[chr] = "init"
+                    with open(jsonPath, 'w') as openfile:
+                        json.dump(json_object, openfile)
+
+            else:
+                print("User does not exist")
+                json_object = None
+                with open(jsonPath, 'w') as openfile:
+                    json_object = {chr: "init"}
+                    json.dump(json_object, openfile)
+
+        if user_input == "chatStop":
+            if os.path.exists(jsonPath):
+                os.remove(jsonPath)
+                print("File exists, and has been deleted")
+                char_ctxt = "File exists, and has been deleted"
+            else:
+                print("No file to delete")
+                char_ctxt = "No file to delete"
+
+        out = [jsonPath, char_ctxt, user_id]
+        return out
+
+    # @modal.method() 
+    def converse(self, mode, chr, char_ctxt, user_input):
+        if mode == 1:            
+            output = self.llm(char_ctxt + 
+                    "Respond to this message as your character, {}".format(chr) + "User: " + user_input + " {}: '".format(chr),
+                    max_tokens=64, 
+                    stop=["User:", "\n", "{}:".format(chr), "However"], echo=True)
+            print("\n {}: ".format(chr))
+        elif mode == 2:
+        # This is for the game mode
+            output = self.llm(char_ctxt 
+                    # + """Respond to this message as your character, {}, would """.format(chr) 
+                    + "User: " + user_input + " {} : '".format(chr),
+                    max_tokens=64, 
+                    stop=["User:", "\n", "{}:".format(chr), "However"], echo=True)
+            print("\n {}: ".format(chr))
+
+        outputString = output['choices'][0]["text"].split("{}:".format(chr))[-1].strip("'")
+        
+        userCtx = "User: '{uInput}' {char}: {bInput}".format(uInput = user_input, char = chr, bInput = outputString)
+        
+        out_2 = [userCtx, outputString]
+        return out_2
+    
+    def outJson(self, chr, jsonPath, userCtx, outputString):
+        print("-> Json Block Entered")
+        with open(jsonPath, 'r') as openfile:
+            json_object = json.load(openfile)
+            oldChrData = json_object[chr]
+        
+        # Replaces the init we placed before with blank
+        if oldChrData == "init":
+            oldChrData = ""        
+
+        print("\t -> Adding USERCTX to OCD")
+        oldChrData += userCtx
+        json_object[chr] = oldChrData
+
+        with open(jsonPath, 'w') as openfile:
+            json.dump(json_object, openfile)
+            
+        return outputString
+
+    @modal.method()
+    def masterMethod(self, mode, chr, context, user_input, user_id, qNo):
+        if user_input == "chatStop":
+            ij = self.initJson(chr, context, user_input, user_id, mode)
+            fDic = {"Status": ij[1]}
+        else: 
+            # Mode 1 = Conversation, mode 2 = dating game
+            if mode == 1:
+                ij = self.initJson(chr, context, user_input, user_id, mode)
+                cv = self.converse(mode, chr, ij[1], user_input)
+                oj = self.outJson(chr, ij[0], cv[0], cv[1])
+
+                fDic = {"conversation":oj, "UID": user_id, "bot": chr}
+            if mode == 2:
+                print("\n-----> Mode 2 \n")
+                if qNo == 1:
+                    print("\n---------> Qno = 1 \n")
+                    # 0 for Stella to go first and 1 for User to go first 
+                    rand_int = 0
+                    rand_Q = random.choice(qList)
+                    print("randQ = ", rand_Q)
+
+                    qContext = """User: {} \n {}: """.format(rand_Q, chr)
+
+                    print("================================================= \n\n Final context {}\n\n".format(context + qContext))
+
+                    ij = self.initJson(chr, context, " ", user_id, mode) #init, u_i isn't used
+
+                    print("---------> ij => \n", ij,"\n\n ij[1] ============> \n\n", ij[1])
+
+                    if rand_int == 0:
+                        cv = self.converse(mode, chr, ij[1] + qContext, " ")
+
+                    oj = self.outJson(chr, ij[0], cv[0], cv[1])
+
+                    fDic = {"ques":rand_Q, "conversation":oj, "UID": user_id, "bot": chr}
+
+                elif qNo == 6:
+                    print("\n---------> Qno = 6 \n")
+                    qAnswer = "User: Will you go on a date with me ?".format(chr)
+                    
+                    ij = self.initJson(chr, context, " ", user_id, mode)
+                    cv = self.converse(mode, chr, ij[1] + qAnswer, " ")
+                    oj = self.outJson(chr, ij[0], cv[0], cv[1])
+
+                    fDic = {"A":oj, "UID": user_id, "bot": chr}
+
+                elif qNo > 1 & qNo < 6:
+                    print("\n---------> Qno = other \n")
+                    ij = self.initJson(chr, context, user_input, user_id, mode)
+                    cv = self.converse(mode, chr, ij[1], user_input)
+                    oj = self.outJson(chr, ij[0], cv[0], cv[1])
+
+                    fDic = {"conversation":oj, "UID": user_id, "bot": chr}
+            print("============================================================================ \n\n\n Input Json: \n {} ,\n\n\n Conversation Data: \n {}, \n\n\n Output'd Json: \n {}\n\n\n".format(ij, cv, oj))
+        return fDic
+
+from typing import Dict
+
+@stub.function(container_idle_timeout=600
+               #, shared_volumes={CACHE_DIR: volume}
+               )
+# JSON reqs: mode, botName(chr), user input, user id, q no, ask answer = q no = 6, test mode, chr context (custom context)
+# JSON out: question asked, botName, user ID, bot response
+@modal.web_endpoint(method="POST")
+def cli(dictvar: Dict):
+    dM = depModal()
+    if dictvar["testMode"] == 0:
+        if dictvar["botName"] in contextDef:
+            return dM.masterMethod.call(int(dictvar["mode"]), dictvar["botName"], contextDef[dictvar["botName"]], dictvar["userContext"], dictvar["userId"], int(dictvar["qNo"]))
+        else:
+            return "Error chx0" # character exist = 0
+
+    elif dictvar["testMode"] == 1:
+        if dictvar["chrContext"] == "":
+            return "Empty character context"
+        else:
+            return dM.masterMethod.call(dictvar["mode"], dictvar["botName"], dictvar["chrContext"], dictvar["userContext"], "testUser", 99)
+    
+    else:
+        return "Error wru0" # wrong usage = 0
