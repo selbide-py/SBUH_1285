564f0666bc83043c997a22cfe31d6f9eb85a5e1d
PoyBoi <106402089+PoyBoi@users.noreply.github.com>
2023-09-24 00:47:56 +0530

    yes

--- Scripts/backups/backup.py
+++ Scripts/backups/backup.py
@@ -2,9 +2,10 @@ from llama_cpp import Llama
 
 print("Hey")
 
-llm = Llama(model_path="C:\TheGoodShit\PythonProject\forMindEaseDocumentation\KoboldAI\models\Wizard-Vicuna-13B-Uncensored.ggmlv3.q4_1.bin",
-                    # n_ctx = 4096,
-                    # verbose= True
-                    )
+llm = Llama(model_path= "C:\TheGoodShit\PythonProject\SBUH\llama-2-13b-chat.Q4_K_M.gguf",
+            n_gpu_layers=10
+            # n_ctx = 4096,
+            # verbose= True
+            )
 
 print("Model successfully loaded")
